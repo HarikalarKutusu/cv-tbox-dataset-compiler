5dc15bbba92a00b3f824c0945d443df420d51bbd
Bülent Özden <bulentozden2007@gmail.com>
2022-10-21 23:01:19 +0300

    Add config file

---
+++ config.py
@@ -0,0 +1,8 @@
+# You local configuration
+# Modify the values to point to respective directories in your system
+
+# This is where your split data is
+SRC_BASE_DIR: str = "C:\\GITREPO\\_HK_GITHUB\\common-voice-diversity-check\\experiments"
+
+# Point to your common voice clone
+CV_REPO: str = "C:\\GITREPO\\_AI_VOICE\\_CV\\common-voice\\server\\data"
