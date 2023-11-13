dc2bad064c1aa1875a2eae4469c8189e8e7cbccb
Bülent Özden <bulentozden2007@gmail.com>
2023-11-13 08:11:40 +0300

    Move program parameters to const

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-# You local configuration
+"""Config file for cv-tbox Dataset Compiler"""
+
+# Your local configuration
 # Modify the values to point to respective directories in your system
 
 # This is where your split data is
@@ -12,4 +14,17 @@ SKIP_TEXT_CORPUS: bool = False
 SKIP_REPORTED: bool = False
 SKIP_SPLITS: bool = False
 
+# Should we re-create old version even if they exist?
 FORCE_CREATE_SPLIT_STATS: bool = False
+# Should we re-create the compressed .tar files even if they exist?
+FORCE_CREATE_COMPRESSED: bool = False
+
+# Program parameters
+VERBOSE: bool = False
+FAIL_ON_NOT_FOUND: bool = True
+
+# Debug & Limiters
+DEBUG: bool = False
+DEBUG_PROC_COUNT: int = 1
+DEBUG_CV_VER: list[str] = ["15.0"]
+DEBUG_CV_LC: list[str] = ["tr"]
