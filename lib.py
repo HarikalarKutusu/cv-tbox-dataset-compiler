bdd0f1f1419ba8d94515518b65d93b5372518b60
Bülent Özden <bulentozden2007@gmail.com>
2024-01-18 09:47:43 +0300

    Extend init_dirs & add git functions

--- lib.py
+++ lib.py
@@ -5,13 +5,17 @@ import os
 import sys
 import csv
 import json
+import multiprocessing as mp
 from typing import Literal, Any
 from urllib.request import urlopen
 
 # External dependencies
+from git import Repo
 import pandas as pd
+import psutil
 
 # Module
+from typedef import GitRec
 import const as c
 import conf
 
@@ -22,13 +26,19 @@ import conf
 
 def init_directories(basedir: str) -> None:
     """Creates data directory structures"""
-    all_locales: list[str] = get_locales(c.CV_VERSIONS[-1])
-    os.makedirs(os.path.join(basedir, c.CACHE_DIRNAME), exist_ok=True)
     data_dir: str = os.path.join(basedir, c.DATA_DIRNAME)
+    if os.path.isfile(os.path.join(data_dir, ".gitkeep")):
+        return
+
+    all_locales: list[str] = get_locales(c.CV_VERSIONS[-1])
     for lc in all_locales:
         os.makedirs(os.path.join(data_dir, c.CD_DIRNAME, lc), exist_ok=True)
-        os.makedirs(os.path.join(data_dir, c.RES_DIRNAME, c.TSV_DIRNAME, lc), exist_ok=True)
-        os.makedirs(os.path.join(data_dir, c.RES_DIRNAME, c.JSON_DIRNAME, lc), exist_ok=True)
+        os.makedirs(
+            os.path.join(data_dir, c.RES_DIRNAME, c.TSV_DIRNAME, lc), exist_ok=True
+        )
+        os.makedirs(
+            os.path.join(data_dir, c.RES_DIRNAME, c.JSON_DIRNAME, lc), exist_ok=True
+        )
     for ver in c.CV_VERSIONS:
         ver_lc: list[str] = get_locales(ver)
         for lc in ver_lc:
@@ -44,12 +54,37 @@ def init_directories(basedir: str) -> None:
     # create .gitkeep
     open(os.path.join(data_dir, ".gitkeep"), "a", encoding="utf8").close()
     open(os.path.join(data_dir, c.CD_DIRNAME, ".gitkeep"), "a", encoding="utf8").close()
-    open(os.path.join(data_dir, c.RES_DIRNAME, ".gitkeep"), "a", encoding="utf8").close()
-    open(os.path.join(data_dir, c.RES_DIRNAME, c.TSV_DIRNAME, ".gitkeep"), "a", encoding="utf8").close()
-    open(os.path.join(data_dir, c.RES_DIRNAME, c.JSON_DIRNAME, ".gitkeep"), "a", encoding="utf8").close()
+    open(
+        os.path.join(data_dir, c.RES_DIRNAME, ".gitkeep"), "a", encoding="utf8"
+    ).close()
+    open(
+        os.path.join(data_dir, c.RES_DIRNAME, c.TSV_DIRNAME, ".gitkeep"),
+        "a",
+        encoding="utf8",
+    ).close()
+    open(
+        os.path.join(data_dir, c.RES_DIRNAME, c.JSON_DIRNAME, ".gitkeep"),
+        "a",
+        encoding="utf8",
+    ).close()
     open(os.path.join(data_dir, c.TC_DIRNAME, ".gitkeep"), "a", encoding="utf8").close()
     open(os.path.join(data_dir, c.VC_DIRNAME, ".gitkeep"), "a", encoding="utf8").close()
-    open(os.path.join(basedir, c.CACHE_DIRNAME, ".gitkeep"), "a", encoding="utf8").close()
+
+    # outside common cache
+    os.makedirs(conf.CV_TBOX_CACHE, exist_ok=True)
+    os.makedirs(os.path.join(conf.CV_TBOX_CACHE, c.CLONES_DIRNAME), exist_ok=True)
+    os.makedirs(os.path.join(conf.CV_TBOX_CACHE, c.API_DIRNAME), exist_ok=True)
+    open(os.path.join(conf.CV_TBOX_CACHE, ".gitkeep"), "a", encoding="utf8").close()
+    open(
+        os.path.join(conf.CV_TBOX_CACHE, c.CLONES_DIRNAME, ".gitkeep"),
+        "a",
+        encoding="utf8",
+    ).close()
+    open(
+        os.path.join(conf.CV_TBOX_CACHE, c.API_DIRNAME, ".gitkeep"),
+        "a",
+        encoding="utf8",
+    ).close()
 
 
 #
@@ -105,8 +140,64 @@ def df_int_convert(x: pd.Series) -> Any:
     """Convert columns to int if possible"""
     try:
         return x.astype(int)
-    except ValueError as e: # pylint: disable=W0612
+    except ValueError as e:  # pylint: disable=W0612
         return x
+
+
+#
+# GIT
+#
+
+
+def _git_clone_or_pull(gitrec: GitRec) -> None:
+    """Local multiprocessing sub to clone a single repo or update it by pulling if it exist"""
+    local_repo_path: str = os.path.join(
+        conf.CV_TBOX_CACHE, c.CLONES_DIRNAME, gitrec.repo
+    )
+    git_path: str = f"{c.GITHUB_BASE}{gitrec.user}/{gitrec.repo}.git"
+    if os.path.isdir(local_repo_path):
+        # repo exists, so pull only
+        if conf.VERBOSE:
+            print(f"GIT PULL: {git_path} => {local_repo_path}")
+        repo: Repo = Repo(path=local_repo_path)
+        repo.remotes.origin.pull()
+        if conf.VERBOSE:
+            print(f"FINISHED PULL: {gitrec.repo}")
+    else:
+        # no local repo, so clone
+        if conf.VERBOSE:
+            print(f"GIT CLONE: {git_path} => {local_repo_path}")
+        repo: Repo = Repo.clone_from(
+            url=git_path,
+            to_path=local_repo_path,
+            multi_options=["--single-branch", "--branch", gitrec.branch],
+        )
+        if conf.VERBOSE:
+            print(f"FINISHED CLONING: {gitrec.repo}")
+
+
+def git_clone_or_pull_all() -> None:
+    """Clones all repos or updates them by pulling if they exist - in multiprocessing"""
+    with mp.Pool(psutil.cpu_count(logical=True)) as pool:
+        pool.map(_git_clone_or_pull, c.CLONES)
+
+
+def git_checkout_date(gitrec: GitRec, checkout_date: str) -> None:
+    """Checkouts a cloned repo at the given date"""
+    local_repo_path: str = os.path.join(
+        conf.CV_TBOX_CACHE, c.CLONES_DIRNAME, gitrec.repo
+    )
+    if os.path.isdir(local_repo_path):
+        # repo exists, so we can checkout
+        if conf.VERBOSE:
+            print(f"CHECKOUT: {local_repo_path} at {checkout_date}")
+        repo: Repo = Repo(path=local_repo_path)
+        commit_hash = repo.git.execute(
+            command=f"git rev-list -n 1 --before='{checkout_date}' origin/{gitrec.branch}"
+        )
+        repo.git.execute(command=f"git checkout {commit_hash}")
+    else:
+        print(f"WARNING: Could not find {gitrec.repo}")
 
 
 #
@@ -124,12 +215,15 @@ def is_version_valid(ver: str) -> Literal[True]:
     return True
 
 
-def calc_dataset_prefix(ver: str) -> str:
+def calc_dataset_prefix(
+    ver: str,
+) -> str:
     """Build the dataset string from version (valid for > v4)"""
 
     if is_version_valid(ver):
         inx: int = c.CV_VERSIONS.index(ver)
-        if ver in ["1", "2", "3", "4"]:
+        # if ver in ["1", "2", "3", "4"]:
+        if ver in ["1", "2", "3"]:
             return f"cv-corpus-{ver}"
         return f"cv-corpus-{ver}-{c.CV_DATES[inx]}"
     return ""
@@ -155,8 +249,7 @@ def get_locales(ver: str) -> list[str]:
     locales: list[str] = []
     for loc, _data in jlocales.items():
         locales.append(loc)
-    locales.sort()
-    return locales
+    return sorted(locales)
 
 
 #
