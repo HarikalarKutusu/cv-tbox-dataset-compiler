25735c419f7284ccba45e0242dc4a220fbfbc875
Bülent Özden <bulentozden2007@gmail.com>
2024-03-24 06:06:51 +0300

    Add old version TC calculations

--- text_corpus_compile.py
+++ text_corpus_compile.py
@@ -18,6 +18,7 @@
 # Standard Lib
 import sys
 import os
+import glob
 import multiprocessing as mp
 
 # External dependencies
@@ -33,6 +34,7 @@ from lib import (
     calc_dataset_prefix,
     df_read,
     df_write,
+    get_cutoff_date,
     get_locales_from_cv_dataset,
     git_checkout,
     git_clone_or_pull_all,
@@ -61,9 +63,13 @@ g: Globals = Globals(
     total_algo=len(c.ALGORITHMS),
 )
 
+#
+# LAST VERSION HANDLERS
+#
 
-def handle_locale(ver_lc: str) -> None:
-    """Process to handle a single locale"""
+
+def handle_last_version_locale(ver_lc: str) -> None:
+    """Process to handle a single locale in last version"""
 
     def handle_preprocess(df_base: pd.DataFrame, df_new: pd.DataFrame) -> pd.DataFrame:
         """Get whole data and only process the unprocessed ones, returns the full result"""
@@ -84,7 +90,9 @@ def handle_locale(ver_lc: str) -> None:
                 validator.validate(s) if isinstance(s, str) else None
                 for s in df_new["sentence"].tolist()
             ]
-            df_new["valid"] = [0 if n is None else 1 for n in df_new["normalized"].tolist()]
+            df_new["valid"] = [
+                0 if n is None else 1 for n in df_new["normalized"].tolist()
+            ]
             df_new["tokens"] = [
                 None if s is None else tokeniser.tokenise(s)
                 for s in df_new["normalized"].tolist()
@@ -117,9 +125,7 @@ def handle_locale(ver_lc: str) -> None:
     )
 
     # cvu - do we have them?
-    validator: cvu.Validator | None = (
-        cvu.Validator(lc) if lc in VALIDATORS else None
-    )
+    validator: cvu.Validator | None = cvu.Validator(lc) if lc in VALIDATORS else None
     phonemiser: cvu.Phonemiser | None = (
         cvu.Phonemiser(lc) if lc in PHONEMISERS else None
     )
@@ -136,7 +142,7 @@ def handle_locale(ver_lc: str) -> None:
 
 
 def handle_last_version() -> None:
-    """Handle a CV version"""
+    """Handle last CV version"""
 
     # Get the repo at cutoff date ([TODO] Need to compile real cut-off dates)
     ver: str = c.CV_VERSIONS[-1]
@@ -148,13 +154,11 @@ def handle_last_version() -> None:
     total_locales: int = len(lc_list)
 
     # Filter out already processed
-    tc_analysis_dir: str = os.path.join(
-        HERE, c.DATA_DIRNAME, c.TC_ANALYSIS_DIRNAME, calc_dataset_prefix(ver)
-    )
+    tc_base_dir: str = os.path.join(HERE, c.DATA_DIRNAME, c.TC_DIRNAME)
     ver_lc_list: list[str] = [
         f"{ver}|{lc}"
         for lc in lc_list
-        if not os.path.isdir(os.path.join(tc_analysis_dir, lc))
+        if not os.path.isfile(os.path.join(tc_base_dir, lc, f"{c.TEXT_CORPUS_FN}_{ver}.tsv"))
         or conf.FORCE_CREATE_TC_STATS
     ]
     num_locales: int = len(ver_lc_list)
@@ -173,7 +177,115 @@ def handle_last_version() -> None:
         with mp.Pool(PROC_COUNT) as pool:
             with tqdm(total=num_locales, desc="") as pbar:
                 for _ in pool.imap_unordered(
-                    handle_locale, ver_lc_list, chunksize=chunk_size
+                    handle_last_version_locale, ver_lc_list, chunksize=chunk_size
+                ):
+                    pbar.update()
+
+    g.total_lc += total_locales
+    g.processed_ver += 1
+    g.processed_lc += num_locales
+    g.skipped_exists += total_locales - num_locales
+
+
+#
+# OLD VERSION HANDLERS
+#
+
+
+def handle_old_version_locale(ver_lc: str) -> None:
+    """Process to handle a single locale in older versions"""
+
+    # handle_locale MAIN
+    ver: str = ver_lc.split("|")[0]
+    lc: str = ver_lc.split("|")[1]
+    ver_dir: str = calc_dataset_prefix(ver)
+
+    # precalc dir and file paths
+    base_tc_dir: str = os.path.join(HERE, c.DATA_DIRNAME, c.TC_DIRNAME, lc)
+    base_tc_file: str = os.path.join(base_tc_dir, f"{c.TEXT_CORPUS_FN}.tsv")
+    ver_tc_file: str = os.path.join(base_tc_dir, f"{c.TEXT_CORPUS_FN}_{ver}.tsv")
+    ver_vc_dir: str = os.path.join(HERE, c.DATA_DIRNAME, c.VC_DIRNAME, ver_dir, lc)
+
+    # get existing base (already preprocessed) and new validated dataframes
+    df_base: pd.DataFrame = df_read(base_tc_file)
+    df: pd.DataFrame = pd.DataFrame(columns=["sentence_id"])
+    # For versions v17.0 and later, we just use the main text_corpora file - even current is generated
+    if float(ver) >= 17.0:
+        df = pd.DataFrame(df_base["sentence_id"]).drop_duplicates().dropna().sort_values("sentence_id")
+        df_write(df, ver_tc_file)
+        return
+
+    # ELSE- For versions before v17.0, get the data from github clone + main buckets
+    # These do not have "sentence_id" field, thus we need to use the "sentence" field to locate them
+
+    # Get sentences from major buckets
+    sentences: list[str] = []
+    for bucket in c.MAIN_BUCKETS:
+        ver_vc_bucket_file: str = os.path.join(ver_vc_dir, f"{bucket}.tsv")
+        if os.path.isfile(ver_vc_bucket_file):
+            df_temp: pd.DataFrame = df_read(ver_vc_bucket_file)
+            sentences.extend(df_temp["sentence"].to_list())
+    # remove duplicates
+    sentences = list(set(sentences))
+
+    # get sentences from git clone server/data/<lc>/*.txt
+    git_data_path: str = os.path.join(
+        conf.CV_TBOX_CACHE, "clones", "common-voice", "server", "data", lc
+    )
+    file_list: list[str] = glob.glob(
+        os.path.join(git_data_path, "*.txt"), recursive=False
+    )
+    for fn in file_list:
+        with open(fn, encoding="utf8") as fd:
+            sentences.extend(fd.readlines())
+    # re-remove duplicates
+    sentences = list(set(sentences))
+
+    # Now get the subset
+    df_subset: pd.DataFrame = df_base[df_base["sentence"].isin(sentences)].drop_duplicates()
+    df_sen_id: pd.DataFrame = df_subset["sentence_id"].to_frame().dropna().sort_values("sentence_id")
+
+    df_write(df_sen_id, ver_tc_file)  # write-out result
+
+
+def handle_older_version(ver: str) -> None:
+    """Handle an older CV version - just keep sentence_id's in the result"""
+
+    # Get the repo at cutoff date ([TODO] Need to compile real cut-off dates)
+    cutoff_date: str = get_cutoff_date(ver)
+    print(f"=== HANDLE: v{ver} @ {cutoff_date} ===")
+
+    lc_list: list[str] = get_locales_from_cv_dataset(ver)
+    total_locales: int = len(lc_list)
+
+    # Filter out already processed
+    base_tc_dir: str = os.path.join(HERE, c.DATA_DIRNAME, c.TC_DIRNAME)
+    ver_lc_list: list[str] = [
+        f"{ver}|{lc}"
+        for lc in lc_list
+        if not os.path.isfile(
+            os.path.join(base_tc_dir, lc, f"{c.TEXT_CORPUS_FN}_{ver}.tsv")
+        )
+        or conf.FORCE_CREATE_TC_STATS
+    ]
+    num_locales: int = len(ver_lc_list)
+
+    # Handle remaining locales in multi-processing
+    chunk_size: int = min(
+        MAX_BATCH_SIZE,
+        num_locales // PROC_COUNT + (0 if num_locales % PROC_COUNT == 0 else 1),
+    )
+    print(
+        f"Total: {total_locales} Existing: {total_locales-num_locales} Remaining: {num_locales} "
+        + f"Procs: {PROC_COUNT}  chunk_size: {chunk_size}..."
+    )
+
+    if num_locales > 0:
+        git_checkout(c.CV_GITREC, cutoff_date)
+        with mp.Pool(PROC_COUNT) as pool:
+            with tqdm(total=num_locales, desc="") as pbar:
+                for _ in pool.imap_unordered(
+                    handle_old_version_locale, ver_lc_list, chunksize=chunk_size
                 ):
                     pbar.update()
 
@@ -193,17 +305,15 @@ def main() -> None:
     git_checkout(c.CV_GITREC)
     git_clone_or_pull_all()
 
-    # Main loop for all versions - start from newest to get latest text corpus data
-    # cv_version_reversed: list[str] = c.CV_VERSIONS.copy()
-    # cv_version_reversed.reverse()
-    # for inx, ver in enumerate(cv_version_reversed):
-    #     handle_version(inx, ver)
-
     # Do it only for last version (after v17.0)
     handle_last_version()
 
+    # Loop for versions, just to keep sentence_id info
+    for ver in c.CV_VERSIONS:
+        handle_older_version(ver)
+
     # done, revert to main and report
-    # git_checkout(c.CV_GITREC)
+    git_checkout(c.CV_GITREC)
     report_results(g)
 
 
